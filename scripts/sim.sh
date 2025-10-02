25243b8e942b90c7a68245e5663dac853745a385
Tipriest <a1503741059@163.com>
2025-10-02 15:48:29 +0800

    perf: optimize sim.sh startup pipeline

--- scripts/sim.sh
+++ scripts/sim.sh
@@ -2,7 +2,7 @@
 # 脚本名称: takeoff_land
 # 脚本描述: 该脚本为起飞&降落控制demo启动脚本,包含PX4 SITL,Gazebo仿真环境,无人机控制节点以及起飞&降落控制节点
 
-gnome-terminal --window -e  'bash -c " sleep 5;
+gnome-terminal --window -e  'bash -c " sleep 1;
                     source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;
                     roslaunch unitree_guide sim.launch; 
                     exec bash"' \
