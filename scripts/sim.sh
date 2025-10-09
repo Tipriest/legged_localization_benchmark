5884328f0ff2fb52a937b4c642d09768467519eb
Tipriest <a1503741059@163.com>
2025-10-09 20:51:15 +0800

    chore

--- scripts/sim.sh
+++ scripts/sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 脚本名称: takeoff_land
-# 脚本描述: 该脚本为起飞&降落控制demo启动脚本,包含PX4 SITL,Gazebo仿真环境,无人机控制节点以及起飞&降落控制节点
+# 脚本名称: 
+# 脚本描述: 
 
 gnome-terminal --window --title="Simulation" -e  'bash -c " sleep 1;
                     source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;
@@ -10,7 +10,7 @@ gnome-terminal --window --title="Simulation" -e  'bash -c " sleep 1;
                     source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;  
                     roslaunch joystick_node joystick_node.launch;
                     exec bash"' \
---tab --title="SLAM" -e 'bash -c " sleep 6;  
-                    source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;  
-                    roslaunch unitree_guide slam.launch;
-                    exec bash"' \
+# --tab --title="SLAM" -e 'bash -c " sleep 6;  
+#                     source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;  
+#                     roslaunch unitree_guide slam.launch;
+#                     exec bash"' \
