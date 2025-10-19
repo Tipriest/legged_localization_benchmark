b4a35b286ff6c5d58ff251137c10c4928127a4a5
Tipriest <a1503741059@163.com>
2025-10-19 23:12:23 +0800

    update for git submodule

--- scripts/slam.sh
+++ scripts/slam.sh
@@ -9,4 +9,12 @@ gnome-terminal --window --title="Simulation" -e  'bash -c " sleep 1;
 --tab --title="Joystick Node" -e 'bash -c " sleep 6;  
                     source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;  
                     roslaunch joystick_node joystick_node.launch;
-                    exec bash"'
+                    exec bash"' \
+--tab --title="SLAM" -e 'bash -c " sleep 12;
+                    source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;  
+                    roslaunch unitree_guide slam.launch;
+                    exec bash"' \
+--tab --title="TFPub" -e 'bash -c " sleep 13;
+                    source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;  
+                    roslaunch unitree_guide tf_pub.launch;
+                    exec bash"' \
