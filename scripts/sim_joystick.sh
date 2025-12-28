20d5d9465b328a568d819298c5485efd3c0ae5e7
Tipriest <a1503741059@163.com>
2025-12-28 17:39:13 +0800

    split simulator and control to different terminals

--- scripts/sim_joystick.sh
+++ scripts/sim_joystick.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# 脚本名称: 
-# 脚本描述: 
+# 脚本名称:
+# 脚本描述:
 
 gnome-terminal --window --title="Simulation" -e  'bash -c " sleep 1;
                     source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;
                     roslaunch unitree_guide sim.launch;
                     exec bash"' \
---tab --title="Robot Input Node" -e 'bash -c " sleep 0;  
-                    source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;  
+--tab --title="Control" -e 'bash -c " sleep 1;
+                    source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;
+                    roslaunch unitree_guide control.launch;
+                    exec bash"' \
+--tab --title="Robot Input Node" -e 'bash -c " sleep 0;
+                    source ~/Documents/legged_localization_benchmark/scripts/environment_setup.sh;
                     roslaunch unitree_guide input.launch input_method:=joystick;
                     exec bash"'
